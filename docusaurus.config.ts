4f4c8fbcfcce2fdf9421c5df4085c3527b97e92b
Colin Platt <ColinPlatt@users.noreply.github.com>
2025-09-22 12:53:42 +0200

    fix: move intro to root & social card (#30)
    * move intro to root
    
    * fix/adds_og_image
    
    * chore: linting
    
    * fix:concepts linking
    
    * fix: rename index
    
    * fix: add index

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -41,6 +41,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           routeBasePath: "/",
           showLastUpdateTime: true,
+
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/MakinaHQ/docs/tree/main/",
@@ -66,7 +67,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/opengraph-image.png",
     navbar: {
       title: "Makina Docs",
       logo: {
@@ -107,7 +108,7 @@ const config: Config = {
           items: [
             {
               label: "Concepts",
-              to: "/intro",
+              to: "/concepts/introduction",
             },
           ],
         },
