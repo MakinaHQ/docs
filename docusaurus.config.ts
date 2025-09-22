37bdd14820ea5b9abc82e788a9190ddb4555fdb9
ColinPlatt <colingplatt@gmail.com>
2025-09-22 12:02:20 +0200

    move intro to root

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -41,6 +41,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           routeBasePath: "/",
           showLastUpdateTime: true,
+          
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/MakinaHQ/docs/tree/main/",
